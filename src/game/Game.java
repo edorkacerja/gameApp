7861d717731032a3b7244ffa129e59ada981b3c0
Edor Kacerja <edorkacerja@gmail.com>
2016-04-15 22:49:54 +0300

    first draft of game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,42 +5,48 @@
  */
 package game;
 
+import java.util.Random;
+
 
 public class Game {
     
     
-    private Player [] players = new Player[5];
-    private java.util.Random rnd = new java.util.Random();
+    private Player[] playersArray = new Player[5];
+    private Random rnd = new Random();
     private int numberPlayers;
     
-    public Game(int numberPlayers){
-        this.numberPlayers = numberPlayers;
+    public Game(){
         //Warm up random number generator
         for (int i=0; i<50000; i++){
             rnd.nextDouble();
         }
-
-
-
-        //Create four Player objects
-        players[1] = new Player(1, rnd);
-        players[2] = new Player(2, rnd);
-        players[3] = new Player(3, rnd);
-        players[4] = new Player(4, rnd);
         
-//        Assign random Response type to each player
-        for(int playerNumber = 1; playerNumber <= numberPlayers; playerNumber++){
-            assignRandomResponse(players[playerNumber]);
-        }
+        playersArray[1] = new Player(1, playersArray);
+        playersArray[2] = new Player(1, playersArray);
+        playersArray[3] = new Player(1, playersArray);
+        playersArray[4] = new Player(1, playersArray);
+        
+    }
+    
+    public void play(){
+        
+        outerloop: do{
+            for(int i=1; i<=4; i++){
+                playersArray[i].updatePosition(rnd.nextInt(6)+1);
+                if(playersArray[i].iWon()){
+                    break outerloop;
+                }
+            }
+        }while(true);
+        
     }
     
     
-    
-    
-    
-    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Game edorsGame = new Game();
+        edorsGame.play();
+
     }
     
 }
